8b076e7c7468c30587f1f81a2c6030c63f7a156f
Yahe <hello@yahe.sh>
2020-04-21 21:17:43 +0200

    removed unnecessary configuration

--- config.php
+++ config.php
@@ -27,63 +27,58 @@
   Handlers::set("podcast_type",        "episodic");
 
   // theme configuration
-  Themes::set("copyright_html",  fhtml("<div class=\"small text-center text-muted\">%s</div>".NL.
-                                       "<div class=\"small text-center\">".NL.
-                                       "  <p class=\"text-muted\">".NL.
-                                       "    <a href=\"%s\">%s</a> · <a href=\"%s\">%s</a> · <a href=\"%s\">%s</a><br>".NL.
-                                       "    <a href=\"%s\">%s</a> · <a href=\"%s\">%s</a>".NL.
-                                       "  </p>".NL.
-                                       "  <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"%s\" title=\"%s\"><i style=\"font-size:2.5rem\" class=\"text-secondary fa fa-instagram mx-2\" aria-hidden=\"true\"></i></a>".NL.
-                                       "  <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"%s\" title=\"%s\"><i style=\"font-size:2.5rem\" class=\"text-secondary fa fa-youtube mx-2\" aria-hidden=\"true\"></i></a>".NL.
-                                       "  <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"%s\" title=\"%s\"><i style=\"font-size:2.5rem\" class=\"text-secondary fa fa-twitter mx-2\" aria-hidden=\"true\"></i></a>".NL.
-                                       "  <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"%s\" title=\"%s\"><i style=\"font-size:2.5rem\" class=\"text-secondary fa fa-facebook mx-2\" aria-hidden=\"true\"></i></a>".NL.
-                                       "</div>",
-                                       "#Filmmakers4Future",
-                                       "/legal",
-                                       "Legal Disclosure",
-                                       "/privacy",
-                                       "Privacy Policy",
-                                       "/contact",
-                                       "Contact",
-                                       "/newsletter",
-                                       "Manage Newsletter",
-                                       "/verify",
-                                       "Verify Signature",
-                                       "https://www.instagram.com/filmmakers4future/",
-                                       "Filmmakers4Future on Instagram",
-                                       "https://www.youtube.com/channel/UC-SNT4gGFgRiFb2iBccJDkw/",
-                                       "Filmmakers4Future on YouTube",
-                                       "https://twitter.com/Filmmakers4F",
-                                       "Filmmakers4Future on Twitter",
-                                       "https://facebook.com/filmmakersforfuture",
-                                       "Filmmakers4Future on Facebook"));
-  Themes::set("header_name",     "Blog");
-  Themes::set("header_sentence", "Green film production in audio form! yeay");
-  Themes::set(MENU,              [[TITLE => "Statement",
-                                   URI   => "/#statement"],
-                                  [TITLE => "Demands",
-                                   URI   => "/demands"],
-                                  [TITLE => "Signatures",
-                                   URI   => "/signatures"],
-                                  [TITLE => "Green Filmmaking",
-                                   URI   => "/greenfilmmaking"],
-                                  [TITLE => "Videos",
-                                   URI   => "/videos"],
-                                  [TITLE => "Participate",
-                                   URI   => "/participate"],
-#                                  [TITLE => "Services",
-#                                   URI   => "#",
-#                                   MENU  => [[TITLE => "Action",
-#                                              URI   => "#"],
-#                                             [TITLE => "Another action",
-#                                              URI   => "#"],
-#                                             [TITLE => "Something else here",
-#                                              URI   => "#"]]],
-                                  [TITLE => "Sign the Statement",
-                                   URI   => "/#sign"]]);
-  Themes::set(SITENAME,          "Filmmakers for Future");
-  Themes::set(SITESLOGAN,        "Blog");
-  Themes::set(TIMEFORMAT,        "d F Y");
+  Themes::set("copyright_html", fhtml("<div class=\"small text-center text-muted\">%s</div>".NL.
+                                      "<div class=\"small text-center\">".NL.
+                                      "  <p class=\"text-muted\">".NL.
+                                      "    <a href=\"%s\">%s</a> · <a href=\"%s\">%s</a> · <a href=\"%s\">%s</a><br>".NL.
+                                      "    <a href=\"%s\">%s</a> · <a href=\"%s\">%s</a>".NL.
+                                      "  </p>".NL.
+                                      "  <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"%s\" title=\"%s\"><i style=\"font-size:2.5rem\" class=\"text-secondary fa fa-instagram mx-2\" aria-hidden=\"true\"></i></a>".NL.
+                                      "  <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"%s\" title=\"%s\"><i style=\"font-size:2.5rem\" class=\"text-secondary fa fa-youtube mx-2\" aria-hidden=\"true\"></i></a>".NL.
+                                      "  <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"%s\" title=\"%s\"><i style=\"font-size:2.5rem\" class=\"text-secondary fa fa-twitter mx-2\" aria-hidden=\"true\"></i></a>".NL.
+                                      "  <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"%s\" title=\"%s\"><i style=\"font-size:2.5rem\" class=\"text-secondary fa fa-facebook mx-2\" aria-hidden=\"true\"></i></a>".NL.
+                                      "</div>",
+                                      "#Filmmakers4Future",
+                                      "/legal",
+                                      "Legal Disclosure",
+                                      "/privacy",
+                                      "Privacy Policy",
+                                      "/contact",
+                                      "Contact",
+                                      "/newsletter",
+                                      "Manage Newsletter",
+                                      "/verify",
+                                      "Verify Signature",
+                                      "https://www.instagram.com/filmmakers4future/",
+                                      "Filmmakers4Future on Instagram",
+                                      "https://www.youtube.com/channel/UC-SNT4gGFgRiFb2iBccJDkw/",
+                                      "Filmmakers4Future on YouTube",
+                                      "https://twitter.com/Filmmakers4F",
+                                      "Filmmakers4Future on Twitter",
+                                      "https://facebook.com/filmmakersforfuture",
+                                      "Filmmakers4Future on Facebook"));
+  Themes::set(MENU,             [[TITLE => "Statement",
+                                  URI   => "/#statement"],
+                                 [TITLE => "Demands",
+                                  URI   => "/demands"],
+                                 [TITLE => "Signatures",
+                                  URI   => "/signatures"],
+                                 [TITLE => "Green Filmmaking",
+                                  URI   => "/greenfilmmaking"],
+                                 [TITLE => "Videos",
+                                  URI   => "/videos"],
+                                 [TITLE => "Participate",
+                                  URI   => "/participate"],
+#                                 [TITLE => "Services",
+#                                  URI   => "#",
+#                                  MENU  => [[TITLE => "Action",
+#                                             URI   => "#"],
+#                                            [TITLE => "Another action",
+#                                             URI   => "#"],
+#                                            [TITLE => "Something else here",
+#                                             URI   => "#"]]],
+                                 [TITLE => "Sign the Statement",
+                                  URI   => "/#sign"]]);
 
   // include the configuration for the Filmmakers for Future handler
   function configure_fm4fhandler() {
