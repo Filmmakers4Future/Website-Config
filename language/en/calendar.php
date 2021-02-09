d94ecb1b79140bd4f3287bb622c8adefd0a1d38f
Paul-Vincent Roll <paviro@me.com>
2021-02-09 13:08:01 +0100

    Added events to calendar

--- language/en/calendar.php
+++ language/en/calendar.php
@@ -15,4 +15,70 @@ Plugins::set("CALENDAR_DATA", [
       "Conference",
       "Online"
   ]],
+  ["title" => "Green Consultant: Korina Gutsche",
+    "date" => "19/02/2021 19:00:00",
+    "timezone" => "Europe/Berlin",
+    "summary" => "The Green Consultant is where all the threads come together. This profession is also a vocation! Sustainability is in Korina's blood.",
+    "image" => "/user/uploads/img/posts/events/talks/2021/korina_gutsche.jpg",
+    "link" => "/en/posts/events/2021-02-08-hands-on-talks-korina-gutsche/",
+    "tags" => [
+      "Hands-on talks",
+      "Green Consulting",
+      "Online"
+  ]],
+  ["title" => "Set Designer: Olivier Meidinger, VSK",
+    "date" => "26/02/2021 19:30:00",
+    "timezone" => "Europe/Berlin",
+    "summary" => "A lot of \"green\" can happen in the art department: Reuse of old film sets (or parts of them), use of new kinds of materials. Rethinking is the order of the day! Olivier tells us how he does it.",
+    "image" => "/user/uploads/img/posts/events/talks/2021/olivier_meidinger.jpg",
+    "link" => "/en/posts/events/2021-02-08-hands-on-talks-olivier-meidinger/",
+    "tags" => [
+      "Hands-on talks",
+      "Set Designer",
+      "Online"
+  ]],
+  ["title" => "Light Designer: Adriano Grilli",
+    "date" => "05/03/2021 19:30:00",
+    "timezone" => "Europe/Berlin",
+    "summary" => "When it comes to light, many people first think of LEDs. But is that really the case? And is that really all there is to it? Adriano will enlighten us on the subject.",
+    "image" => "/user/uploads/img/posts/events/talks/2021/adriano_grilli.jpg",
+    "link" => "/en/posts/events/2021-02-08-hands-on-talks-adriano-grilli/",
+    "tags" => [
+      "Hands-on talks",
+      "Light Designer",
+      "Online"
+  ]],
+  ["title" => "Director: Lars Jessen",
+    "date" => "12/03/2021 19:30:00",
+    "timezone" => "Europe/Berlin",
+    "summary" => "Direction and \"green\"? How does that come together? Lars tells us how much influence the director has on the sustainability of the entire production process.",
+    "image" => "/user/uploads/img/posts/events/talks/2021/jessen_lars_c_marion_von_der_mehden.jpg",
+    "link" => "/en/posts/events/2021-02-08-hands-on-talks-lars-jessen/",
+    "tags" => [
+      "Hands-on talks",
+      "Director",
+      "Online"
+  ]],
+  ["title" => "Costume Designer: Tini Fetscher",
+    "date" => "19/03/2021 19:30:00",
+    "timezone" => "Europe/Berlin",
+    "summary" => "Many have already heard about the grim environmental footprint of the fashion industry. Tini tells us how to significantly improve the footprint in the costume sector. Or should we say: she tells us all about it?",
+    "image" => "/user/uploads/img/posts/events/talks/2021/tini_fetscher.jpg",
+    "link" => "/en/posts/events/2021-02-08-hands-on-talks-tini-fetscher/",
+    "tags" => [
+      "Hands-on talks",
+      "Costume Designer",
+      "Online"
+  ]],
+  ["title" => "Post Production: Carsten Schuffert",
+    "date" => "26/03/2021 19:30:00",
+    "timezone" => "Europe/Berlin",
+    "summary" => "The last step in film production - when the (hopefully sustainably!) shot material arrives at this point, a surprising amount of \"green\" is possible! Carsten tells us what all that is.",
+    "image" => "/user/uploads/img/posts/events/talks/2021/carsten_schuffert.jpg",
+    "link" => "/en/posts/events/2021-02-08-hands-on-talks-carsten-schuffert/",
+    "tags" => [
+      "Hands-on talks",
+      "Post Production",
+      "Online"
+  ]],
 ]);
